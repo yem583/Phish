232495291164d7af561ac4ab0b9d7466bcf0839f
Jim Keeley <jkeeley@tsp-consulting.com>
2019-04-23 06:59:21 -0400

    add caching to songdataservice

--- Phish.ApiClient/Phish.HttpClient/SongDataService.cs
+++ Phish.ApiClient/Phish.HttpClient/SongDataService.cs
@@ -22,77 +22,91 @@ namespace Phish.HttpClient
 
         public async Task<IEnumerable<Song>> GetSongsAsync()
         {
-            var response = await Client.GetAsync("song");
-            var result = await response.Content.ReadAsStringAsync();
-            response.EnsureSuccessStatusCode();
-            var doc = new HtmlDocument();
-            doc.LoadHtml(result);
-
-            var list = new List<Song>();
-            var venueRowNodes = doc.DocumentNode.SelectNodes("//tr");
-            foreach (var songRowNode in venueRowNodes.Skip(1).ToList())
+            if (!MemoryCache.TryGetValue("_HttpSongs", out IEnumerable<Song> cacheEntry))
             {
-                var classes = songRowNode.Attributes["class"].Value.Split(' ');
-                var songCells = songRowNode.ChildNodes.Where(c => c.Name == "td").ToList();
-                var nameCell = songCells[0];
-                var songUrl = nameCell.ChildNodes[0].Attributes["href"].Value;
-                var originalArtistCell = songCells[1];
-                var originalArtistUrl = originalArtistCell.ChildNodes.Any() &&
-                                        originalArtistCell.ChildNodes[0].Name == "a" ?
-                    originalArtistCell.ChildNodes[0].Attributes["href"].Value : null;
-                var timesCell = songCells[2];
-                var timesUrl = timesCell.ChildNodes.Any() &&
-                               timesCell.ChildNodes[0].Name == "a" ? 
-                    timesCell.ChildNodes[0].Attributes["href"].Value:null;
-                string debutLink = null;
-                HtmlNode gapCell=null;
-                string lastLink = null;
-                HtmlNode lastCell = null;
-                HtmlNode debutCell = null;
-                if (songCells.Count > 3)
+                var response = await Client.GetAsync("song");
+                var result = await response.Content.ReadAsStringAsync();
+                response.EnsureSuccessStatusCode();
+                var doc = new HtmlDocument();
+                doc.LoadHtml(result);
+
+                var list = new List<Song>();
+                var venueRowNodes = doc.DocumentNode.SelectNodes("//tr");
+                foreach (var songRowNode in venueRowNodes.Skip(1).ToList())
                 {
-                    debutCell = songCells[3];
-                    debutLink = debutCell.ChildNodes.Any() &&
-                                    debutCell.ChildNodes[0].Name == "a" ?
-                        debutCell.ChildNodes[0].Attributes["href"].Value : null;
-                    lastCell = songCells[4];
-                    lastLink = lastCell.ChildNodes.Any() &&
-                                   lastCell.ChildNodes[0].Name == "a" ?
-                        lastCell.ChildNodes[0].Attributes["href"].Value : null;
-                    gapCell = songCells[5];
+                    var classes = songRowNode.Attributes["class"].Value.Split(' ');
+                    var songCells = songRowNode.ChildNodes.Where(c => c.Name == "td").ToList();
+                    var nameCell = songCells[0];
+                    var songUrl = nameCell.ChildNodes[0].Attributes["href"].Value;
+                    var originalArtistCell = songCells[1];
+                    var originalArtistUrl = originalArtistCell.ChildNodes.Any() &&
+                                            originalArtistCell.ChildNodes[0].Name == "a"
+                        ? originalArtistCell.ChildNodes[0].Attributes["href"].Value
+                        : null;
+                    var timesCell = songCells[2];
+                    var timesUrl = timesCell.ChildNodes.Any() &&
+                                   timesCell.ChildNodes[0].Name == "a"
+                        ? timesCell.ChildNodes[0].Attributes["href"].Value
+                        : null;
+                    string debutLink = null;
+                    HtmlNode gapCell = null;
+                    string lastLink = null;
+                    HtmlNode lastCell = null;
+                    HtmlNode debutCell = null;
+                    if (songCells.Count > 3)
+                    {
+                        debutCell = songCells[3];
+                        debutLink = debutCell.ChildNodes.Any() &&
+                                    debutCell.ChildNodes[0].Name == "a"
+                            ? debutCell.ChildNodes[0].Attributes["href"].Value
+                            : null;
+                        lastCell = songCells[4];
+                        lastLink = lastCell.ChildNodes.Any() &&
+                                   lastCell.ChildNodes[0].Name == "a"
+                            ? lastCell.ChildNodes[0].Attributes["href"].Value
+                            : null;
+                        gapCell = songCells[5];
+                    }
+
+                    var song = new Song()
+                    {
+                        SongName = nameCell?.InnerText,
+                        DebutLink = debutLink,
+                        Gap = gapCell != null && !string.IsNullOrEmpty(gapCell.InnerText)
+                            ? int.Parse(gapCell.InnerText)
+                            : (int?) null,
+                        LastLink = lastLink,
+                        OriginalArtist = originalArtistCell?.InnerText,
+                        OriginalArtistLink = originalArtistUrl,
+                        Times = timesCell?.InnerText,
+                        TimesLink = timesUrl,
+                        IsAlias = classes.Contains("aliases"),
+                        IsCover = classes.Contains("covers"),
+                        IsOriginal = classes.Contains("originals"),
+                        SongLink = songUrl
+                    };
+                    if (lastCell != null && DateTime.TryParse(lastCell.InnerText, out var songLast))
+                    {
+                        song.Last = songLast;
+                    }
+
+                    if (debutCell != null && DateTime.TryParse(debutCell.InnerText, out var songDebut))
+                    {
+                        song.Debut = songDebut;
+                    }
+
+                    if (gapCell != null && int.TryParse(gapCell.InnerText, out var songGap))
+                    {
+                        song.Gap = songGap;
+                    }
+
+                    list.Add(song);
                 }
-              
-                var song = new Song()
-                {
-                    SongName = nameCell?.InnerText,
-                    DebutLink = debutLink,
-                    Gap = gapCell != null && !string.IsNullOrEmpty(gapCell.InnerText) ? int.Parse(gapCell.InnerText):(int?)null,
-                    LastLink = lastLink,
-                    OriginalArtist = originalArtistCell?.InnerText,
-                    OriginalArtistLink = originalArtistUrl,
-                    Times = timesCell?.InnerText,
-                    TimesLink = timesUrl,
-                    IsAlias = classes.Contains("aliases"),
-                    IsCover = classes.Contains("covers"),
-                    IsOriginal = classes.Contains("originals"),
-                    SongLink = songUrl
-                };
-                if (lastCell != null && DateTime.TryParse(lastCell.InnerText, out var songLast))
-                {
-                    song.Last = songLast;
-                }
-                if (debutCell != null && DateTime.TryParse(debutCell.InnerText, out var songDebut))
-                {
-                    song.Debut = songDebut;
-                }
-                if (gapCell != null && int.TryParse(gapCell.InnerText, out var songGap))
-                {
-                    song.Gap = songGap;
-                }
-                list.Add(song);
+                cacheEntry = list;
             }
 
-            return list;
+            return cacheEntry;
+
         }
     }
 }
