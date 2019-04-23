62f01baa2830b2f3e80d0648513e721960649596
Jim Keeley <jkeeley@tsp-consulting.com>
2019-04-23 04:57:01 -0400

    Add Song Domain OBject

---
+++ Phish.Domain/Song.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace Phish.Domain
+{
+    public class Song
+    {
+        public string SongName{get; set;}
+
+        public string OriginalArtist { get; set; }
+
+        public string Times { get; set; }
+
+        public DateTime? Debut { get; set; }
+
+        public DateTime? Last { get; set; }
+
+        public int? Gap { get; set; }
+    }
+}
