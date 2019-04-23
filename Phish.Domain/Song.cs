10bcdcc07a399a0287c31725dfc5f0dd171e717b
Jim Keeley <jkeeley@tsp-consulting.com>
2019-04-23 05:06:30 -0400

    add more song properties

--- Phish.Domain/Song.cs
+++ Phish.Domain/Song.cs
@@ -6,13 +6,23 @@ namespace Phish.Domain
     {
         public string SongName{get; set;}
 
+        public string SongLink { get; set; }
+
         public string OriginalArtist { get; set; }
+
+        public string OriginalArtistLink { get; set; }
 
         public string Times { get; set; }
 
+        public string TimeLink { get; set; }
+
         public DateTime? Debut { get; set; }
 
+        public string DebutLink { get; set; }
+
         public DateTime? Last { get; set; }
+
+        public string LastLink { get; set; }
 
         public int? Gap { get; set; }
 
