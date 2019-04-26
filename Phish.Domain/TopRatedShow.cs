039169810c3314d24fdb139a1f7e91de8f3470bf
Jim Keeley <jkeeley@tsp-consulting.com>
2019-04-25 20:20:09 -0400

    add topratedshow

---
+++ Phish.Domain/TopRatedShow.cs
@@ -0,0 +1,23 @@
+﻿using System;
+
+namespace Phish.Domain
+{
+    public class TopRatedShow
+    {
+        public decimal? Rating { get; set; }
+
+        public int? NumberOfVotes { get; set; }
+
+        public DateTime? ShowDate { get; set; }
+
+        public string ShowDateUrl { get; set; }
+
+        public string Venue { get; set; }
+
+        public string City { get; set; }
+
+        public string State { get; set; }
+
+        public string Country { get; set; }
+    }
+}
